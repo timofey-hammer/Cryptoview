991738b8083f9934b114c764ae38ef84ba1641ff
Timofey Hammer <myservices.hammer@gmail.com>
2024-01-10 16:18:03 +0300

    LaunchScreen

--- Cryptoview/Extensions/Color.swift
+++ Cryptoview/Extensions/Color.swift
@@ -8,6 +8,7 @@ import SwiftUI
 
 extension Color {
     static let theme = ColorTheme()
+    static let launch = LaunchTheme()
 }
 
 struct ColorTheme {
@@ -16,4 +17,9 @@ struct ColorTheme {
     let green = Color("GreenColor")
     let red = Color("RedColor")
     let secondaryText = Color("SecondaryTextColor")
+}
+
+struct LaunchTheme {
+    let accent = Color("LaunchAccentColor")
+    let background = Color("LaunchBackgroundColor")
 }
